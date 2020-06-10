aaa6bb3326739c2216a26294aece97f6946d997c
tifu3080 <13508647@student.uts.edu.au>
2020-06-10 20:33:17 +1000

    Add Speech Convertion Tableview

---
+++ iMemo/speechConvertViewController.swift
@@ -0,0 +1,39 @@
+//
+//  speechConvertViewController.swift
+//  iMemo
+//
+//  Created by admin on 10/6/20.
+//  Copyright © 2020 Enterprise. All rights reserved.
+//
+
+import UIKit
+import Speech
+
+class speechConvertViewController: UIViewController {
+    
+    
+
+    
+    
+    @IBOutlet weak var convertSpeech: UIButton!
+    @IBOutlet weak var convertedText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
