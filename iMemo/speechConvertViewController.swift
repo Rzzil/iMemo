ad21ef7288820b3b08b982db0cce20a18f014697
tifu3080 <13508647@student.uts.edu.au>
2020-06-10 20:51:35 +1000

    speech conversion view controller update
    updated view controller with table view and pointer working

--- iMemo/speechConvertViewController.swift
+++ iMemo/speechConvertViewController.swift
@@ -9,18 +9,53 @@
 import UIKit
 import Speech
 
-class speechConvertViewController: UIViewController {
+class speechConvertViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    let manager = FileManager.default
+    let url = NSHomeDirectory() + "/Documents/"
+    
+    var cintents1 = [String]()
+    var currentCell: UITableViewCell?
     
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cintents1.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        if(indexPath.row != 0)
+        {
+            cell.textLabel?.text = cintents1[indexPath.row]
+        }
+        return cell
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currentCell = tableView.cellForRow(at: indexPath)
+        print("测试:" + (currentCell?.textLabel?.text ?? ""))
+    }
+    
+        
     @IBOutlet weak var convertSpeech: UIButton!
     @IBOutlet weak var convertedText: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        let domain = Bundle.main.bundleIdentifier!
+        //        UserDefaults.standard.removePersistentDomain(forName: domain)
+                print("地址："+url)
+                do
+                {
+                    cintents1 = try manager.contentsOfDirectory(atPath: url)
+                    print("cintents:\(cintents1)\n")
+                } catch{
+                    print("Error occurs.")
+                }
+                
+                // Do any additional setup after loading the view.
 
         // Do any additional setup after loading the view.
     }
